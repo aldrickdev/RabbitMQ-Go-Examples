3db0b8ecd2f340f0de8a0546efe44269f4105b13
aldrick.castro <aldrick.castro@datadoghq.com>
2023-11-12 10:56:37 -0500

    Added examples 01-03

---
+++ cmd/03-pubsub/producer/producer.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "Hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
+}
+
+func main() {
+	// connect to RabbitMQ
+	conn, err := amqp.Dial("amqp://admin:admin@broker:5672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// Open a channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// create an exhange
+	err = ch.ExchangeDeclare(
+		"logs",   // name
+		"fanout", // type
+		true,     //durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := bodyFrom(os.Args)
+	err = ch.PublishWithContext(
+		ctx,
+		"logs", // exchance
+		"",     // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
